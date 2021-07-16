e04194bc1c8e4f20afe5a3b6b91564da719d4b59
Angelica <angelicahui@gmail.com>
2021-07-16 17:30:25 -0400

    modals 7 to 12

--- index.js
+++ index.js
@@ -49,3 +49,99 @@ var tnmn4c = document.getElementById("tnmn-4-c");
 tnmn4c.onclick = function() {
   tnmn4m.style.display = "none";
 }
+
+var tnmn1m = document.getElementById("tnmn-1-m");
+var tnmn1tr = document.getElementById("tnmn-1-tr");
+var tnmn1mc = document.getElementById("tnmn-1-mc");
+tnmn1tr.onclick = function(){
+  tnmn1m.style.display = "block";
+  tnmn1mc.src = this.src;
+}
+var tnmn1c = document.getElementById("tnmn-1-c");
+tnmn1c.onclick = function() {
+  tnmn1m.style.display = "none";
+}
+
+var tnmn6m = document.getElementById("tnmn-6-m");
+var tnmn6tr = document.getElementById("tnmn-6-tr");
+var tnmn6mc = document.getElementById("tnmn-6-mc");
+tnmn6tr.onclick = function(){
+  tnmn6m.style.display = "block";
+  tnmn6mc.src = this.src;
+}
+var tnmn6c = document.getElementById("tnmn-6-c");
+tnmn6c.onclick = function() {
+  tnmn6m.style.display = "none";
+}
+
+var tnmn7m = document.getElementById("tnmn-7-m");
+var tnmn7tr = document.getElementById("tnmn-7-tr");
+var tnmn7mc = document.getElementById("tnmn-7-mc");
+tnmn7tr.onclick = function(){
+  tnmn7m.style.display = "block";
+  tnmn7mc.src = this.src;
+}
+var tnmn7c = document.getElementById("tnmn-7-c");
+tnmn7c.onclick = function() {
+  tnmn7m.style.display = "none";
+}
+
+var tnmn8m = document.getElementById("tnmn-8-m");
+var tnmn8tr = document.getElementById("tnmn-8-tr");
+var tnmn8mc = document.getElementById("tnmn-8-mc");
+tnmn8tr.onclick = function(){
+  tnmn8m.style.display = "block";
+  tnmn8mc.src = this.src;
+}
+var tnmn8c = document.getElementById("tnmn-8-c");
+tnmn8c.onclick = function() {
+  tnmn8m.style.display = "none";
+}
+
+var tnmn9m = document.getElementById("tnmn-9-m");
+var tnmn9tr = document.getElementById("tnmn-9-tr");
+var tnmn9mc = document.getElementById("tnmn-9-mc");
+tnmn9tr.onclick = function(){
+  tnmn9m.style.display = "block";
+  tnmn9mc.src = this.src;
+}
+var tnmn9c = document.getElementById("tnmn-9-c");
+tnmn9c.onclick = function() {
+  tnmn9m.style.display = "none";
+}
+
+var tnmn10m = document.getElementById("tnmn-10-m");
+var tnmn10tr = document.getElementById("tnmn-10-tr");
+var tnmn10mc = document.getElementById("tnmn-10-mc");
+tnmn10tr.onclick = function(){
+  tnmn10m.style.display = "block";
+  tnmn10mc.src = this.src;
+}
+var tnmn10c = document.getElementById("tnmn-10-c");
+tnmn10c.onclick = function() {
+  tnmn10m.style.display = "none";
+}
+
+var tnmn11m = document.getElementById("tnmn-11-m");
+var tnmn11tr = document.getElementById("tnmn-11-tr");
+var tnmn11mc = document.getElementById("tnmn-11-mc");
+tnmn11tr.onclick = function(){
+  tnmn11m.style.display = "block";
+  tnmn11mc.src = this.src;
+}
+var tnmn11c = document.getElementById("tnmn-11-c");
+tnmn11c.onclick = function() {
+  tnmn11m.style.display = "none";
+}
+
+var tnmn12m = document.getElementById("tnmn-12-m");
+var tnmn12tr = document.getElementById("tnmn-12-tr");
+var tnmn12mc = document.getElementById("tnmn-12-mc");
+tnmn12tr.onclick = function(){
+  tnmn12m.style.display = "block";
+  tnmn12mc.src = this.src;
+}
+var tnmn12c = document.getElementById("tnmn-12-c");
+tnmn12c.onclick = function() {
+  tnmn12m.style.display = "none";
+}
