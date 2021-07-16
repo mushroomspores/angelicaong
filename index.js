54738228aaebdeefab768b974c018af2b496a98d
Angelica <angelicahui@gmail.com>
2021-07-16 01:34:58 -0400

    modal images!!

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const saicpcc1920cap = document.querySelector('#saicpcc1920cap')
-const saicpcc1920img = document.querySelector('#saicpcc1920img')
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function changeColour() {saicpcc1920cap.style.color = 'grey'}
-function revertColour() {saicpcc1920cap.style.color = 'black'}
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
 
-saicpcc1920img.addEventListener('mouseover', changeColour)
-saicpcc1920img.addEventListener('mouseleave', revertColour)
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
