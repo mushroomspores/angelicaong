089d80741d88584cd8ec2c989e986f06e3e8d509
Angelica <angelicahui@gmail.com>
2021-07-22 13:35:57 -0400

    reverie page created

---
+++ ph-reverie.js
@@ -0,0 +1,196 @@
+//js script for ph-veneer.html
+// Get the modal
+var rv1m = document.getElementById("rv-1-m");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var rv1tr = document.getElementById("rv-1-tr");
+var rv1mc = document.getElementById("rv-1-mc");
+rv1tr.onclick = function(){
+  rv1m.style.display = "block";
+  rv1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var rv1c = document.getElementById("rv-1-c");
+// When the user clicks on <span> (x), close the modal
+rv1c.onclick = function() {
+  rv1m.style.display = "none";
+}
+
+var rv2m = document.getElementById("rv-2-m");
+var rv2tr = document.getElementById("rv-2-tr");
+var rv2mc = document.getElementById("rv-2-mc");
+rv2tr.onclick = function(){
+  rv2m.style.display = "block";
+  rv2mc.src = this.src;
+}
+var rv2c = document.getElementById("rv-2-c");
+rv2c.onclick = function() {
+  rv2m.style.display = "none";
+}
+
+var rv3m = document.getElementById("rv-3-m");
+var rv3tr = document.getElementById("rv-3-tr");
+var rv3mc = document.getElementById("rv-3-mc");
+rv3tr.onclick = function(){
+  rv3m.style.display = "block";
+  rv3mc.src = this.src;
+}
+var rv3c = document.getElementById("rv-3-c");
+rv3c.onclick = function() {
+  rv3m.style.display = "none";
+}
+
+var rv4m = document.getElementById("rv-4-m");
+var rv4tr = document.getElementById("rv-4-tr");
+var rv4mc = document.getElementById("rv-4-mc");
+rv4tr.onclick = function(){
+  rv4m.style.display = "block";
+  rv4mc.src = this.src;
+}
+var rv4c = document.getElementById("rv-4-c");
+rv4c.onclick = function() {
+  rv4m.style.display = "none";
+}
+
+var rv5m = document.getElementById("rv-5-m");
+var rv5tr = document.getElementById("rv-5-tr");
+var rv5mc = document.getElementById("rv-5-mc");
+rv5tr.onclick = function(){
+  rv5m.style.display = "block";
+  rv5mc.src = this.src;
+}
+var rv5c = document.getElementById("rv-5-c");
+rv5c.onclick = function() {
+  rv5m.style.display = "none";
+}
+
+var rv6m = document.getElementById("rv-6-m");
+var rv6tr = document.getElementById("rv-6-tr");
+var rv6mc = document.getElementById("rv-6-mc");
+rv6tr.onclick = function(){
+  rv6m.style.display = "block";
+  rv6mc.src = this.src;
+}
+var rv6c = document.getElementById("rv-6-c");
+rv6c.onclick = function() {
+  rv6m.style.display = "none";
+}
+
+var rv7m = document.getElementById("rv-7-m");
+var rv7tr = document.getElementById("rv-7-tr");
+var rv7mc = document.getElementById("rv-7-mc");
+rv7tr.onclick = function(){
+  rv7m.style.display = "block";
+  rv7mc.src = this.src;
+}
+var rv7c = document.getElementById("rv-7-c");
+rv7c.onclick = function() {
+  rv7m.style.display = "none";
+}
+
+var rv8m = document.getElementById("rv-8-m");
+var rv8tr = document.getElementById("rv-8-tr");
+var rv8mc = document.getElementById("rv-8-mc");
+rv8tr.onclick = function(){
+  rv8m.style.display = "block";
+  rv8mc.src = this.src;
+}
+var rv8c = document.getElementById("rv-8-c");
+rv8c.onclick = function() {
+  rv8m.style.display = "none";
+}
+
+var rv9m = document.getElementById("rv-9-m");
+var rv9tr = document.getElementById("rv-9-tr");
+var rv9mc = document.getElementById("rv-9-mc");
+rv9tr.onclick = function(){
+  rv9m.style.display = "block";
+  rv9mc.src = this.src;
+}
+var rv9c = document.getElementById("rv-9-c");
+rv9c.onclick = function() {
+  rv9m.style.display = "none";
+}
+
+var rv10m = document.getElementById("rv-10-m");
+var rv10tr = document.getElementById("rv-10-tr");
+var rv10mc = document.getElementById("rv-10-mc");
+rv10tr.onclick = function(){
+  rv10m.style.display = "block";
+  rv10mc.src = this.src;
+}
+var rv10c = document.getElementById("rv-10-c");
+rv10c.onclick = function() {
+  rv10m.style.display = "none";
+}
+
+var rv11m = document.getElementById("rv-11-m");
+var rv11tr = document.getElementById("rv-11-tr");
+var rv11mc = document.getElementById("rv-11-mc");
+rv11tr.onclick = function(){
+  rv11m.style.display = "block";
+  rv11mc.src = this.src;
+}
+var rv11c = document.getElementById("rv-11-c");
+rv11c.onclick = function() {
+  rv11m.style.display = "none";
+}
+
+var rv12m = document.getElementById("rv-12-m");
+var rv12tr = document.getElementById("rv-12-tr");
+var rv12mc = document.getElementById("rv-12-mc");
+rv12tr.onclick = function(){
+  rv12m.style.display = "block";
+  rv12mc.src = this.src;
+}
+var rv12c = document.getElementById("rv-12-c");
+rv12c.onclick = function() {
+  rv12m.style.display = "none";
+}
+
+var rv13m = document.getElementById("rv-13-m");
+var rv13tr = document.getElementById("rv-13-tr");
+var rv13mc = document.getElementById("rv-13-mc");
+rv13tr.onclick = function(){
+  rv13m.style.display = "block";
+  rv13mc.src = this.src;
+}
+var rv13c = document.getElementById("rv-13-c");
+rv13c.onclick = function() {
+  rv13m.style.display = "none";
+}
+
+var rv14m = document.getElementById("rv-14-m");
+var rv14tr = document.getElementById("rv-14-tr");
+var rv14mc = document.getElementById("rv-14-mc");
+rv14tr.onclick = function(){
+  rv14m.style.display = "block";
+  rv14mc.src = this.src;
+}
+var rv14c = document.getElementById("rv-14-c");
+rv14c.onclick = function() {
+  rv14m.style.display = "none";
+}
+
+var rv15m = document.getElementById("rv-15-m");
+var rv15tr = document.getElementById("rv-15-tr");
+var rv15mc = document.getElementById("rv-15-mc");
+rv15tr.onclick = function(){
+  rv15m.style.display = "block";
+  rv15mc.src = this.src;
+}
+var rv15c = document.getElementById("rv-15-c");
+rv15c.onclick = function() {
+  rv15m.style.display = "none";
+}
+
+var rv16m = document.getElementById("rv-16-m");
+var rv16tr = document.getElementById("rv-16-tr");
+var rv16mc = document.getElementById("rv-16-mc");
+rv16tr.onclick = function(){
+  rv16m.style.display = "block";
+  rv16mc.src = this.src;
+}
+var rv16c = document.getElementById("rv-16-c");
+rv16c.onclick = function() {
+  rv16m.style.display = "none";
+}
