18b195e92647026d58d0a2abaeefd6fce0db89d1
Angelica <angelicahui@gmail.com>
2021-07-22 15:56:48 -0400

    a whisper and a silhouette page made

---
+++ ph-awhisperandasilhouette.js
@@ -0,0 +1,196 @@
+//js script for ph-veneer.html
+// Get the modal
+var awaas1m = document.getElementById("awaas-1-m");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var awaas1tr = document.getElementById("awaas-1-tr");
+var awaas1mc = document.getElementById("awaas-1-mc");
+awaas1tr.onclick = function(){
+  awaas1m.style.display = "block";
+  awaas1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var awaas1c = document.getElementById("awaas-1-c");
+// When the user clicks on <span> (x), close the modal
+awaas1c.onclick = function() {
+  awaas1m.style.display = "none";
+}
+
+var awaas2m = document.getElementById("awaas-2-m");
+var awaas2tr = document.getElementById("awaas-2-tr");
+var awaas2mc = document.getElementById("awaas-2-mc");
+awaas2tr.onclick = function(){
+  awaas2m.style.display = "block";
+  awaas2mc.src = photographyimages/awhisperandasilhouette/awhisperandasilhouette-2-2020-angelicaong.jpg.src;
+}
+var awaas2c = document.getElementById("awaas-2-c");
+awaas2c.onclick = function() {
+  awaas2m.style.display = "none";
+}
+
+var awaas3m = document.getElementById("awaas-3-m");
+var awaas3tr = document.getElementById("awaas-3-tr");
+var awaas3mc = document.getElementById("awaas-3-mc");
+awaas3tr.onclick = function(){
+  awaas3m.style.display = "block";
+  awaas3mc.src = photographyimages/awhisperandasilhouette/awhisperandasilhouette-3-2020-angelicaong-borderless.jpg.src;
+}
+var awaas3c = document.getElementById("awaas-3-c");
+awaas3c.onclick = function() {
+  awaas3m.style.display = "none";
+}
+
+var awaas5m = document.getElementById("awaas-5-m");
+var awaas5tr = document.getElementById("awaas-5-tr");
+var awaas5mc = document.getElementById("awaas-5-mc");
+awaas5tr.onclick = function(){
+  awaas5m.style.display = "block";
+  awaas5mc.src = hotographyimages/awhisperandasilhouette/awhisperandasilhouette-5-2020-angelicaong.jpg.src;
+}
+var awaas5c = document.getElementById("awaas-5-c");
+awaas5c.onclick = function() {
+  awaas5m.style.display = "none";
+}
+
+var awaas6m = document.getElementById("awaas-6-m");
+var awaas6tr = document.getElementById("awaas-6-tr");
+var awaas6mc = document.getElementById("awaas-6-mc");
+awaas6tr.onclick = function(){
+  awaas6m.style.display = "block";
+  awaas6mc.src = this.src;
+}
+var awaas6c = document.getElementById("awaas-6-c");
+awaas6c.onclick = function() {
+  awaas6m.style.display = "none";
+}
+
+var awaas7m = document.getElementById("awaas-7-m");
+var awaas7tr = document.getElementById("awaas-7-tr");
+var awaas7mc = document.getElementById("awaas-7-mc");
+awaas7tr.onclick = function(){
+  awaas7m.style.display = "block";
+  awaas7mc.src = this.src;
+}
+var awaas7c = document.getElementById("awaas-7-c");
+awaas7c.onclick = function() {
+  awaas7m.style.display = "none";
+}
+
+var awaas8m = document.getElementById("awaas-8-m");
+var awaas8tr = document.getElementById("awaas-8-tr");
+var awaas8mc = document.getElementById("awaas-8-mc");
+awaas8tr.onclick = function(){
+  awaas8m.style.display = "block";
+  awaas8mc.src = this.src;
+}
+var awaas8c = document.getElementById("awaas-8-c");
+awaas8c.onclick = function() {
+  awaas8m.style.display = "none";
+}
+
+var awaas9m = document.getElementById("awaas-9-m");
+var awaas9tr = document.getElementById("awaas-9-tr");
+var awaas9mc = document.getElementById("awaas-9-mc");
+awaas9tr.onclick = function(){
+  awaas9m.style.display = "block";
+  awaas9mc.src = this.src;
+}
+var awaas9c = document.getElementById("awaas-9-c");
+awaas9c.onclick = function() {
+  awaas9m.style.display = "none";
+}
+
+var awaas10m = document.getElementById("awaas-10-m");
+var awaas10tr = document.getElementById("awaas-10-tr");
+var awaas10mc = document.getElementById("awaas-10-mc");
+awaas10tr.onclick = function(){
+  awaas10m.style.display = "block";
+  awaas10mc.src = this.src;
+}
+var awaas10c = document.getElementById("awaas-10-c");
+awaas10c.onclick = function() {
+  awaas10m.style.display = "none";
+}
+
+var awaas11m = document.getElementById("awaas-11-m");
+var awaas11tr = document.getElementById("awaas-11-tr");
+var awaas11mc = document.getElementById("awaas-11-mc");
+awaas11tr.onclick = function(){
+  awaas11m.style.display = "block";
+  awaas11mc.src = this.src;
+}
+var awaas11c = document.getElementById("awaas-11-c");
+awaas11c.onclick = function() {
+  awaas11m.style.display = "none";
+}
+
+var awaas12m = document.getElementById("awaas-12-m");
+var awaas12tr = document.getElementById("awaas-12-tr");
+var awaas12mc = document.getElementById("awaas-12-mc");
+awaas12tr.onclick = function(){
+  awaas12m.style.display = "block";
+  awaas12mc.src = this.src;
+}
+var awaas12c = document.getElementById("awaas-12-c");
+awaas12c.onclick = function() {
+  awaas12m.style.display = "none";
+}
+
+var awaashandm = document.getElementById("awaas-hand-m");
+var awaashandtr = document.getElementById("awaas-hand-tr");
+var awaashandmc = document.getElementById("awaas-hand-mc");
+awaashandtr.onclick = function(){
+  awaashandm.style.display = "block";
+  awaashandmc.src = this.src;
+}
+var awaashandc = document.getElementById("awaas-hand-c");
+awaashandc.onclick = function() {
+  awaashandm.style.display = "none";
+}
+
+var awaaspearlm = document.getElementById("awaas-pearl-m");
+var awaaspearltr = document.getElementById("awaas-pearl-tr");
+var awaaspearlmc = document.getElementById("awaas-pearl-mc");
+awaaspearltr.onclick = function(){
+  awaaspearlm.style.display = "block";
+  awaaspearlmc.src = this.src;
+}
+var awaaspearlc = document.getElementById("awaas-pearl-c");
+awaaspearlc.onclick = function() {
+  awaaspearlm.style.display = "none";
+}
+
+var awaasthunderm = document.getElementById("awaas-thunder-m");
+var awaasthundertr = document.getElementById("awaas-thunder-tr");
+var awaasthundermc = document.getElementById("awaas-thunder-mc");
+awaasthundertr.onclick = function(){
+  awaasthunderm.style.display = "block";
+  awaasthundermc.src = this.src;
+}
+var awaasthunderc = document.getElementById("awaas-thunder-c");
+awaasthunderc.onclick = function() {
+  awaasthunderm.style.display = "none";
+}
+
+var awaashands1m = document.getElementById("awaas-hands1-m");
+var awaashands1tr = document.getElementById("awaas-hands1-tr");
+var awaashands1mc = document.getElementById("awaas-hands1-mc");
+awaashands1tr.onclick = function(){
+  awaashands1m.style.display = "block";
+  awaashands1mc.src = this.src;
+}
+var awaashands1c = document.getElementById("awaas-hands1-c");
+awaashands1c.onclick = function() {
+  awaashands1m.style.display = "none";
+}
+
+var awaashands2m = document.getElementById("awaas-hands2-m");
+var awaashands2tr = document.getElementById("awaas-hands2-tr");
+var awaashands2mc = document.getElementById("awaas-hands2-mc");
+awaashands2tr.onclick = function(){
+  awaashands2m.style.display = "block";
+  awaashands2mc.src = this.src;
+}
+var awaashands2c = document.getElementById("awaas-hands2-c");
+awaashands2c.onclick = function() {
+  awaashands2m.style.display = "none";
+}
