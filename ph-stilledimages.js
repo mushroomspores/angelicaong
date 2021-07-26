e5ec0b1cfe6085ac54cc1d69431af79f9d8d9947
Angelica <angelicahui@gmail.com>
2021-07-26 04:14:08 -0400

    stilled images update

---
+++ ph-stilledimages.js
@@ -0,0 +1,160 @@
+//js script for ph-veneer.html
+// Get the modal
+var si1m = document.getElementById("si-1-m");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var si1tr = document.getElementById("si-1-tr");
+var si1mc = document.getElementById("si-1-mc");
+si1tr.onclick = function(){
+  si1m.style.display = "block";
+  si1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var si1c = document.getElementById("si-1-c");
+// When the user clicks on <span> (x), close the modal
+si1c.onclick = function() {
+  si1m.style.display = "none";
+}
+
+var si2m = document.getElementById("si-2-m");
+var si2tr = document.getElementById("si-2-tr");
+var si2mc = document.getElementById("si-2-mc");
+si2tr.onclick = function(){
+  si2m.style.display = "block";
+  si2mc.src = photographyimages/awhisperandasilhouette/awhisperandasilhouette-2-2020-angelicaong.jpg.src;
+}
+var si2c = document.getElementById("si-2-c");
+si2c.onclick = function() {
+  si2m.style.display = "none";
+}
+
+var si3m = document.getElementById("si-3-m");
+var si3tr = document.getElementById("si-3-tr");
+var si3mc = document.getElementById("si-3-mc");
+si3tr.onclick = function(){
+  si3m.style.display = "block";
+  si3mc.src = photographyimages/awhisperandasilhouette/awhisperandasilhouette-3-2020-angelicaong-borderless.jpg.src;
+}
+var si3c = document.getElementById("si-3-c");
+si3c.onclick = function() {
+  si3m.style.display = "none";
+}
+
+var si4m = document.getElementById("si-4-m");
+var si4tr = document.getElementById("si-4-tr");
+var si4mc = document.getElementById("si-4-mc");
+si4tr.onclick = function(){
+  si4m.style.display = "block";
+  si4mc.src = photographyimages/awhisperandasilhouette/awhisperandasilhouette-3-2020-angelicaong-borderless.jpg.src;
+}
+var si4c = document.getElementById("si-4-c");
+si4c.onclick = function() {
+  si4m.style.display = "none";
+}
+
+var si5m = document.getElementById("si-5-m");
+var si5tr = document.getElementById("si-5-tr");
+var si5mc = document.getElementById("si-5-mc");
+si5tr.onclick = function(){
+  si5m.style.display = "block";
+  si5mc.src = hotographyimages/awhisperandasilhouette/awhisperandasilhouette-5-2020-angelicaong.jpg.src;
+}
+var si5c = document.getElementById("si-5-c");
+si5c.onclick = function() {
+  si5m.style.display = "none";
+}
+
+var si6m = document.getElementById("si-6-m");
+var si6tr = document.getElementById("si-6-tr");
+var si6mc = document.getElementById("si-6-mc");
+si6tr.onclick = function(){
+  si6m.style.display = "block";
+  si6mc.src = this.src;
+}
+var si6c = document.getElementById("si-6-c");
+si6c.onclick = function() {
+  si6m.style.display = "none";
+}
+
+var si7m = document.getElementById("si-7-m");
+var si7tr = document.getElementById("si-7-tr");
+var si7mc = document.getElementById("si-7-mc");
+si7tr.onclick = function(){
+  si7m.style.display = "block";
+  si7mc.src = this.src;
+}
+var si7c = document.getElementById("si-7-c");
+si7c.onclick = function() {
+  si7m.style.display = "none";
+}
+
+var si8m = document.getElementById("si-8-m");
+var si8tr = document.getElementById("si-8-tr");
+var si8mc = document.getElementById("si-8-mc");
+si8tr.onclick = function(){
+  si8m.style.display = "block";
+  si8mc.src = this.src;
+}
+var si8c = document.getElementById("si-8-c");
+si8c.onclick = function() {
+  si8m.style.display = "none";
+}
+
+var si9m = document.getElementById("si-9-m");
+var si9tr = document.getElementById("si-9-tr");
+var si9mc = document.getElementById("si-9-mc");
+si9tr.onclick = function(){
+  si9m.style.display = "block";
+  si9mc.src = this.src;
+}
+var si9c = document.getElementById("si-9-c");
+si9c.onclick = function() {
+  si9m.style.display = "none";
+}
+
+var si10m = document.getElementById("si-10-m");
+var si10tr = document.getElementById("si-10-tr");
+var si10mc = document.getElementById("si-10-mc");
+si10tr.onclick = function(){
+  si10m.style.display = "block";
+  si10mc.src = this.src;
+}
+var si10c = document.getElementById("si-10-c");
+si10c.onclick = function() {
+  si10m.style.display = "none";
+}
+
+var sidappledm = document.getElementById("si-dappled-m");
+var sidappledtr = document.getElementById("si-dappled-tr");
+var sidappledmc = document.getElementById("si-dappled-mc");
+sidappledtr.onclick = function(){
+  sidappledm.style.display = "block";
+  sidappledmc.src = this.src;
+}
+var sidappledc = document.getElementById("si-dappled-c");
+sidappledc.onclick = function() {
+  sidappledm.style.display = "none";
+}
+
+var sithroughtheglassm = document.getElementById("si-throughtheglass-m");
+var sithroughtheglasstr = document.getElementById("si-throughtheglass-tr");
+var sithroughtheglassmc = document.getElementById("si-throughtheglass-mc");
+sithroughtheglasstr.onclick = function(){
+  sithroughtheglassm.style.display = "block";
+  sithroughtheglassmc.src = this.src;
+}
+var sithroughtheglassc = document.getElementById("si-throughtheglass-c");
+sithroughtheglassc.onclick = function() {
+  sithroughtheglassm.style.display = "none";
+}
+
+var sicasinom = document.getElementById("si-casino-m");
+var sicasinotr = document.getElementById("si-casino-tr");
+var sicasinomc = document.getElementById("si-casino-mc");
+sicasinotr.onclick = function(){
+  sicasinom.style.display = "block";
+  sicasinomc.src = this.src;
+}
+var sicasinoc = document.getElementById("si-casino-c");
+sicasinoc.onclick = function() {
+  sicasinom.style.display = "none";
+}
