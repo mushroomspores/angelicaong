78b1278481eb46081a1d8cc607e61a021c97f40a
Angelica <angelicahui@gmail.com>
2021-07-27 02:09:02 -0400

    asoad up

---
+++ in-astudyofaday.js
@@ -0,0 +1,52 @@
+//js script for ph-veneer.html
+// Get the modal
+var asoad1m = document.getElementById("asoad-1-m");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var asoad1tr = document.getElementById("asoad-1-tr");
+var asoad1mc = document.getElementById("asoad-1-mc");
+asoad1tr.onclick = function(){
+  asoad1m.style.display = "block";
+  asoad1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var asoad1c = document.getElementById("asoad-1-c");
+// When the user clicks on <span> (x), close the modal
+asoad1c.onclick = function() {
+  asoad1m.style.display = "none";
+}
+
+var asoad2m = document.getElementById("asoad-2-m");
+var asoad2tr = document.getElementById("asoad-2-tr");
+var asoad2mc = document.getElementById("asoad-2-mc");
+asoad2tr.onclick = function(){
+  asoad2m.style.display = "block";
+  asoad2mc.src = this.src;
+}
+var asoad2c = document.getElementById("asoad-2-c");
+asoad2c.onclick = function() {
+  asoad2m.style.display = "none";
+}
+
+var asoad3m = document.getElementById("asoad-3-m");
+var asoad3tr = document.getElementById("asoad-3-tr");
+var asoad3mc = document.getElementById("asoad-3-mc");
+asoad3tr.onclick = function(){
+  asoad3m.style.display = "block";
+  asoad3mc.src = this.src;
+}
+var asoad3c = document.getElementById("asoad-3-c");
+asoad3c.onclick = function() {
+  asoad3m.style.display = "none";
+}
+
+var asoad4m = document.getElementById("asoad-4-m");
+var asoad4tr = document.getElementById("asoad-4-tr");
+var asoad4mc = document.getElementById("asoad-4-mc");
+asoad4tr.onclick = function(){
+  asoad4m.style.display = "block";
+  asoad4mc.src = this.src;
+}
+var asoad4c = document.getElementById("asoad-4-c");
+  asoad4c.onclick = function() {
+  asoad4m.style.display = "none";
+}
