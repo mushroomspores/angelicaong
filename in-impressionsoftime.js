d35b94a4f1c430d4cfe915346d4164f301e224fb
Angelica <angelicahui@gmail.com>
2021-07-27 21:02:41 -0400

    iot done

---
+++ in-impressionsoftime.js
@@ -0,0 +1,109 @@
+//js script for ph-veneer.html
+// Get the modal
+var iot1m = document.getElementById("iot-1-m");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var iot1tr = document.getElementById("iot-1-tr");
+var iot1mc = document.getElementById("iot-1-mc");
+iot1tr.onclick = function(){
+  iot1m.style.display = "block";
+  iot1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var iot1c = document.getElementById("iot-1-c");
+// When the user clicks on <span> (x), close the modal
+iot1c.onclick = function() {
+  iot1m.style.display = "none";
+}
+iot1m.onclick = function() {
+  iot1m.style.display = "none";
+}
+
+var iot2m = document.getElementById("iot-2-m");
+var iot2tr = document.getElementById("iot-2-tr");
+var iot2mc = document.getElementById("iot-2-mc");
+iot2tr.onclick = function(){
+  iot2m.style.display = "block";
+  iot2mc.src = this.src;
+}
+var iot2c = document.getElementById("iot-2-c");
+iot2c.onclick = function() {
+  iot2m.style.display = "none";
+}
+iot2m.onclick = function() {
+  iot2m.style.display = "none";
+}
+
+var iot3m = document.getElementById("iot-3-m");
+var iot3tr = document.getElementById("iot-3-tr");
+var iot3mc = document.getElementById("iot-3-mc");
+iot3tr.onclick = function(){
+  iot3m.style.display = "block";
+  iot3mc.src = this.src;
+}
+var iot3c = document.getElementById("iot-3-c");
+iot3c.onclick = function() {
+  iot3m.style.display = "none";
+}
+iot3m.onclick = function() {
+  iot3m.style.display = "none";
+}
+
+var iot4m = document.getElementById("iot-4-m");
+var iot4tr = document.getElementById("iot-4-tr");
+var iot4mc = document.getElementById("iot-4-mc");
+iot4tr.onclick = function(){
+  iot4m.style.display = "block";
+  iot4mc.src = this.src;
+}
+var iot4c = document.getElementById("iot-4-c");
+iot4c.onclick = function() {
+  iot4m.style.display = "none";
+}
+iot4m.onclick = function() {
+  iot4m.style.display = "none";
+}
+
+var iot5m = document.getElementById("iot-5-m");
+var iot5tr = document.getElementById("iot-5-tr");
+var iot5mc = document.getElementById("iot-5-mc");
+iot5tr.onclick = function(){
+  iot5m.style.display = "block";
+  iot5mc.src = this.src;
+}
+var iot5c = document.getElementById("iot-5-c");
+iot5c.onclick = function() {
+  iot5m.style.display = "none";
+}
+iot5m.onclick = function() {
+  iot5m.style.display = "none";
+}
+
+var iot6m = document.getElementById("iot-6-m");
+var iot6tr = document.getElementById("iot-6-tr");
+var iot6mc = document.getElementById("iot-6-mc");
+iot6tr.onclick = function(){
+  iot6m.style.display = "block";
+  iot6mc.src = this.src;
+}
+var iot6c = document.getElementById("iot-6-c");
+iot6c.onclick = function() {
+  iot6m.style.display = "none";
+}
+iot6m.onclick = function() {
+  iot6m.style.display = "none";
+}
+
+var iot7m = document.getElementById("iot-7-m");
+var iot7tr = document.getElementById("iot-7-tr");
+var iot7mc = document.getElementById("iot-7-mc");
+iot7tr.onclick = function(){
+  iot7m.style.display = "block";
+  iot7mc.src = this.src;
+}
+var iot7c = document.getElementById("iot-7-c");
+iot7c.onclick = function() {
+  iot7m.style.display = "none";
+}
+iot7m.onclick = function() {
+  iot7m.style.display = "none";
+}
