f5d4d48af2024931f360d9864c4d2448ac619c4e
Angelica <angelicahui@gmail.com>
2021-08-02 14:32:45 -0400

    pnt trm ii done

---
+++ pn-trm-ii.js
@@ -0,0 +1,124 @@
+//js script for ph-veneer.html
+// Get the modal
+var trmii1m = document.getElementById("trm-ii-1-m");
+// Get the image and insert it intrmiide the modal - use its "alt" text as a caption
+var trmii1tr = document.getElementById("trm-ii-1-tr");
+var trmii1mc = document.getElementById("trm-ii-1-mc");
+trmii1tr.onclick = function(){
+  trmii1m.style.display = "block";
+  trmii1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var trmii1c = document.getElementById("trm-ii-1-c");
+// When the user clicks on <span> (x), close the modal
+trmii1c.onclick = function() {
+  trmii1m.style.display = "none";
+}
+trmii1m.onclick = function() {
+  trmii1m.style.display = "none";
+}
+
+var trmii2m = document.getElementById("trm-ii-2-m");
+var trmii2tr = document.getElementById("trm-ii-2-tr");
+var trmii2mc = document.getElementById("trm-ii-2-mc");
+trmii2tr.onclick = function(){
+  trmii2m.style.display = "block";
+  trmii2mc.src = photographyimages/awhisperandatrmiilhouette/awhisperandatrmiilhouette-2-2020-angelicaong.jpg.src;
+}
+var trmii2c = document.getElementById("trm-ii-2-c");
+trmii2c.onclick = function() {
+  trmii2m.style.display = "none";
+}
+trmii2m.onclick = function() {
+  trmii2m.style.display = "none";
+}
+
+var trmii3m = document.getElementById("trm-ii-3-m");
+var trmii3tr = document.getElementById("trm-ii-3-tr");
+var trmii3mc = document.getElementById("trm-ii-3-mc");
+trmii3tr.onclick = function(){
+  trmii3m.style.display = "block";
+  trmii3mc.src = photographyimages/awhisperandatrmiilhouette/awhisperandatrmiilhouette-3-2020-angelicaong-borderless.jpg.src;
+}
+var trmii3c = document.getElementById("trm-ii-3-c");
+trmii3c.onclick = function() {
+  trmii3m.style.display = "none";
+}
+trmii3m.onclick = function() {
+  trmii3m.style.display = "none";
+}
+
+var trmii4m = document.getElementById("trm-ii-4-m");
+var trmii4tr = document.getElementById("trm-ii-4-tr");
+var trmii4mc = document.getElementById("trm-ii-4-mc");
+trmii4tr.onclick = function(){
+  trmii4m.style.display = "block";
+  trmii4mc.src = photographyimages/awhisperandatrmiilhouette/awhisperandatrmiilhouette-3-2020-angelicaong-borderless.jpg.src;
+}
+var trmii4c = document.getElementById("trm-ii-4-c");
+trmii4c.onclick = function() {
+  trmii4m.style.display = "none";
+}
+trmii4m.onclick = function() {
+  trmii4m.style.display = "none";
+}
+
+var trmii5m = document.getElementById("trm-ii-5-m");
+var trmii5tr = document.getElementById("trm-ii-5-tr");
+var trmii5mc = document.getElementById("trm-ii-5-mc");
+trmii5tr.onclick = function(){
+  trmii5m.style.display = "block";
+  trmii5mc.src = hotographyimages/awhisperandatrmiilhouette/awhisperandatrmiilhouette-5-2020-angelicaong.jpg.src;
+}
+var trmii5c = document.getElementById("trm-ii-5-c");
+trmii5c.onclick = function() {
+  trmii5m.style.display = "none";
+}
+trmii5m.onclick = function() {
+  trmii5m.style.display = "none";
+}
+
+var trmii6m = document.getElementById("trm-ii-6-m");
+var trmii6tr = document.getElementById("trm-ii-6-tr");
+var trmii6mc = document.getElementById("trm-ii-6-mc");
+trmii6tr.onclick = function(){
+  trmii6m.style.display = "block";
+  trmii6mc.src = this.src;
+}
+var trmii6c = document.getElementById("trm-ii-6-c");
+trmii6c.onclick = function() {
+  trmii6m.style.display = "none";
+}
+trmii6m.onclick = function() {
+  trmii6m.style.display = "none";
+}
+
+var trmii7m = document.getElementById("trm-ii-7-m");
+var trmii7tr = document.getElementById("trm-ii-7-tr");
+var trmii7mc = document.getElementById("trm-ii-7-mc");
+trmii7tr.onclick = function(){
+  trmii7m.style.display = "block";
+  trmii7mc.src = this.src;
+}
+var trmii7c = document.getElementById("trm-ii-7-c");
+trmii7c.onclick = function() {
+  trmii7m.style.display = "none";
+}
+trmii7m.onclick = function() {
+  trmii7m.style.display = "none";
+}
+
+var trmii8m = document.getElementById("trm-ii-8-m");
+var trmii8tr = document.getElementById("trm-ii-8-tr");
+var trmii8mc = document.getElementById("trm-ii-8-mc");
+trmii8tr.onclick = function(){
+  trmii8m.style.display = "block";
+  trmii8mc.src = this.src;
+}
+var trmii8c = document.getElementById("trm-ii-8-c");
+trmii8c.onclick = function() {
+  trmii8m.style.display = "none";
+}
+trmii8m.onclick = function() {
+  trmii8m.style.display = "none";
+}
