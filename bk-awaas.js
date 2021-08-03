f634e960f1cbc65c86aeb916b7e090027e73d7d1
Angelica <angelicahui@gmail.com>
2021-08-03 02:09:51 -0400

    bk awaas done

---
+++ bk-awaas.js
@@ -0,0 +1,79 @@
+//js script for ph-veneer.html
+// Get the modal
+var bkawaas1m = document.getElementById("bk-awaas-1-m");
+// Get the image and insert it inbkawaasde the modal - use its "alt" text as a caption
+var bkawaas1tr = document.getElementById("bk-awaas-1-tr");
+var bkawaas1mc = document.getElementById("bk-awaas-1-mc");
+bkawaas1tr.onclick = function(){
+  bkawaas1m.style.display = "block";
+  bkawaas1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var bkawaas1c = document.getElementById("bk-awaas-1-c");
+// When the user clicks on <span> (x), close the modal
+bkawaas1c.onclick = function() {
+  bkawaas1m.style.display = "none";
+}
+bkawaas1m.onclick = function() {
+  bkawaas1m.style.display = "none";
+}
+
+var bkawaas3m = document.getElementById("bk-awaas-3-m");
+var bkawaas3tr = document.getElementById("bk-awaas-3-tr");
+var bkawaas3mc = document.getElementById("bk-awaas-3-mc");
+bkawaas3tr.onclick = function(){
+  bkawaas3m.style.display = "block";
+  bkawaas3mc.src = this.src;
+}
+var bkawaas3c = document.getElementById("bk-awaas-3-c");
+bkawaas3c.onclick = function() {
+  bkawaas3m.style.display = "none";
+}
+bkawaas3m.onclick = function() {
+  bkawaas3m.style.display = "none";
+}
+
+var bkawaas4m = document.getElementById("bk-awaas-4-m");
+var bkawaas4tr = document.getElementById("bk-awaas-4-tr");
+var bkawaas4mc = document.getElementById("bk-awaas-4-mc");
+bkawaas4tr.onclick = function(){
+  bkawaas4m.style.display = "block";
+  bkawaas4mc.src = this.src;
+}
+var bkawaas4c = document.getElementById("bk-awaas-4-c");
+bkawaas4c.onclick = function() {
+  bkawaas4m.style.display = "none";
+}
+bkawaas4m.onclick = function() {
+  bkawaas4m.style.display = "none";
+}
+
+var bkawaas5m = document.getElementById("bk-awaas-5-m");
+var bkawaas5tr = document.getElementById("bk-awaas-5-tr");
+var bkawaas5mc = document.getElementById("bk-awaas-5-mc");
+bkawaas5tr.onclick = function(){
+  bkawaas5m.style.display = "block";
+  bkawaas5mc.src = this.src;
+}
+var bkawaas5c = document.getElementById("bk-awaas-5-c");
+bkawaas5c.onclick = function() {
+  bkawaas5m.style.display = "none";
+}
+bkawaas5m.onclick = function() {
+  bkawaas5m.style.display = "none";
+}
+
+var bkawaas9m = document.getElementById("bk-awaas-9-m");
+var bkawaas9tr = document.getElementById("bk-awaas-9-tr");
+var bkawaas9mc = document.getElementById("bk-awaas-9-mc");
+bkawaas9tr.onclick = function(){
+  bkawaas9m.style.display = "block";
+  bkawaas9mc.src = this.src;
+}
+var bkawaas9c = document.getElementById("bk-awaas-9-c");
+bkawaas9c.onclick = function() {
+  bkawaas9m.style.display = "none";
+}
+bkawaas9m.onclick = function() {
+  bkawaas9m.style.display = "none";
+}
