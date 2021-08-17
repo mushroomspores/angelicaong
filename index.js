538105f86c40ffed4d5fa6cedce08f8a8391cb14
Angelica <angelicahui@gmail.com>
2021-08-16 23:37:25 -0500

    updates

--- index.js
+++ index.js
@@ -1,3 +1,108 @@
 AOS.init({
   duration: 900,
 })
+
+var hmpgtnmn5m = document.getElementById("hmpg-tnmn-5-m");
+var hmpgtnmn5tr = document.getElementById("hmpg-tnmn-5-tr");
+var hmpgtnmn5mc = document.getElementById("hmpg-tnmn-5-mc");
+hmpgtnmn5tr.onclick = function(){
+  hmpgtnmn5m.style.display = "block";
+  hmpgtnmn5mc.src = this.src;
+}
+var hmpgtnmn5c = document.getElementById("hmpg-tnmn-5-c");
+hmpgtnmn5c.onclick = function() {
+  hmpgtnmn5m.style.display = "none";
+}
+hmpgtnmn5m.onclick = function() {
+  hmpgtnmn5m.style.display = "none";
+}
+
+var hmpgtnmn1m = document.getElementById("hmpg-tnmn-1-m");
+var hmpgtnmn1tr = document.getElementById("hmpg-tnmn-1-tr");
+var hmpgtnmn1mc = document.getElementById("hmpg-tnmn-1-mc");
+hmpgtnmn1tr.onclick = function(){
+  hmpgtnmn1m.style.display = "block";
+  hmpgtnmn1mc.src = this.src;
+}
+var hmpgtnmn1c = document.getElementById("hmpg-tnmn-1-c");
+hmpgtnmn1c.onclick = function() {
+  hmpgtnmn1m.style.display = "none";
+}
+hmpgtnmn1m.onclick = function() {
+  hmpgtnmn1m.style.display = "none";
+}
+
+var hmpgtnmn3m = document.getElementById("hmpg-tnmn-3-m");
+var hmpgtnmn3tr = document.getElementById("hmpg-tnmn-3-tr");
+var hmpgtnmn3mc = document.getElementById("hmpg-tnmn-3-mc");
+hmpgtnmn3tr.onclick = function(){
+  hmpgtnmn3m.style.display = "block";
+  hmpgtnmn3mc.src = this.src;
+}
+var hmpgtnmn3c = document.getElementById("hmpg-tnmn-3-c");
+hmpgtnmn3c.onclick = function() {
+  hmpgtnmn3m.style.display = "none";
+}
+hmpgtnmn3m.onclick = function() {
+  hmpgtnmn3m.style.display = "none";
+}
+
+var hmpgtnmn7m = document.getElementById("hmpg-tnmn-7-m");
+var hmpgtnmn7tr = document.getElementById("hmpg-tnmn-7-tr");
+var hmpgtnmn7mc = document.getElementById("hmpg-tnmn-7-mc");
+hmpgtnmn7tr.onclick = function(){
+  hmpgtnmn7m.style.display = "block";
+  hmpgtnmn7mc.src = this.src;
+}
+var hmpgtnmn7c = document.getElementById("hmpg-tnmn-7-c");
+hmpgtnmn7c.onclick = function() {
+  hmpgtnmn7m.style.display = "none";
+}
+hmpgtnmn7m.onclick = function() {
+  hmpgtnmn7m.style.display = "none";
+}
+
+var hmpgpnthunderm = document.getElementById("hmpg-pn-thunder-m");
+var hmpgpnthundertr = document.getElementById("hmpg-pn-thunder-tr");
+var hmpgpnthundermc = document.getElementById("hmpg-pn-thunder-mc");
+hmpgpnthundertr.onclick = function(){
+  hmpgpnthunderm.style.display = "block";
+  hmpgpnthundermc.src = this.src;
+}
+var hmpgpnthunderc = document.getElementById("hmpg-pn-thunder-c");
+hmpgpnthunderc.onclick = function() {
+  hmpgpnthunderm.style.display = "none";
+}
+hmpgpnthunderm.onclick = function() {
+  hmpgpnthunderm.style.display = "none";
+}
+
+var hmpgpntrmiim = document.getElementById("hmpg-pn-trm-ii-m");
+var hmpgpntrmiitr = document.getElementById("hmpg-pn-trm-ii-tr");
+var hmpgpntrmiimc = document.getElementById("hmpg-pn-trm-ii-mc");
+hmpgpntrmiitr.onclick = function(){
+  hmpgpntrmiim.style.display = "block";
+  hmpgpntrmiimc.src = this.src;
+}
+var hmpgpntrmiic = document.getElementById("hmpg-pn-trm-ii-c");
+hmpgpntrmiic.onclick = function() {
+  hmpgpntrmiim.style.display = "none";
+}
+hmpgpntrmiim.onclick = function() {
+  hmpgpntrmiim.style.display = "none";
+}
+
+var hmpgpntianmum = document.getElementById("hmpg-pn-tianmu-m");
+var hmpgpntianmutr = document.getElementById("hmpg-pn-tianmu-tr");
+var hmpgpntianmumc = document.getElementById("hmpg-pn-tianmu-mc");
+hmpgpntianmutr.onclick = function(){
+  hmpgpntianmum.style.display = "block";
+  hmpgpntianmumc.src = this.src;
+}
+var hmpgpntianmuc = document.getElementById("hmpg-pn-tianmu-c");
+hmpgpntianmuc.onclick = function() {
+  hmpgpntianmum.style.display = "none";
+}
+hmpgpntianmum.onclick = function() {
+  hmpgpntianmum.style.display = "none";
+}
