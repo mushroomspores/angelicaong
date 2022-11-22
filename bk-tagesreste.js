39f75ac950cede558bf82e9e4ce51e93cfad57aa
Angelica <angelicahui@gmail.com>
2022-11-22 09:28:42 -0600

    study of a day & tagesreste updates

---
+++ bk-tagesreste.js
@@ -0,0 +1,138 @@
+AOS.init({
+  duration: 900,
+})
+
+var tge1m = document.getElementById("tg-e1-m");
+var tge1tr = document.getElementById("tg-e1-tr");
+var tge1mc = document.getElementById("tg-e1-mc");
+tge1tr.onclick = function(){
+  tge1m.style.display = "block";
+  tge1mc.src = this.src;
+}
+var tge1c = document.getElementById("tg-e1-c");
+tge1c.onclick = function() {
+  tge1m.style.display = "none";
+}
+tge1m.onclick = function() {
+  tge1m.style.display = "none";
+}
+
+var tge5m = document.getElementById("tg-e5-m");
+var tge5tr = document.getElementById("tg-e5-tr");
+var tge5mc = document.getElementById("tg-e5-mc");
+tge5tr.onclick = function(){
+  tge5m.style.display = "block";
+  tge5mc.src = this.src;
+}
+var tge5c = document.getElementById("tg-e5-c");
+tge5c.onclick = function() {
+  tge5m.style.display = "none";
+}
+tge5m.onclick = function() {
+  tge5m.style.display = "none";
+}
+
+var tg1m = document.getElementById("tg-1-m");
+var tg1tr = document.getElementById("tg-1-tr");
+var tg1mc = document.getElementById("tg-1-mc");
+tg1tr.onclick = function(){
+  tg1m.style.display = "block";
+  tg1mc.src = this.src;
+}
+var tg1c = document.getElementById("tg-1-c");
+tg1c.onclick = function() {
+  tg1m.style.display = "none";
+}
+tg1m.onclick = function() {
+  tg1m.style.display = "none";
+}
+
+var tg2m = document.getElementById("tg-2-m");
+var tg2tr = document.getElementById("tg-2-tr");
+var tg2mc = document.getElementById("tg-2-mc");
+tg2tr.onclick = function(){
+  tg2m.style.display = "block";
+  tg2mc.src = this.src;
+}
+var tg2c = document.getElementById("tg-2-c");
+tg2c.onclick = function() {
+  tg2m.style.display = "none";
+}
+tg2m.onclick = function() {
+  tg2m.style.display = "none";
+}
+
+var tg6m = document.getElementById("tg-6-m");
+var tg6tr = document.getElementById("tg-6-tr");
+var tg6mc = document.getElementById("tg-6-mc");
+tg6tr.onclick = function(){
+  tg6m.style.display = "block";
+  tg6mc.src = this.src;
+}
+var tg6c = document.getElementById("tg-6-c");
+tg6c.onclick = function() {
+  tg6m.style.display = "none";
+}
+tg6m.onclick = function() {
+  tg6m.style.display = "none";
+}
+
+var tg8m = document.getElementById("tg-8-m");
+var tg8tr = document.getElementById("tg-8-tr");
+var tg8mc = document.getElementById("tg-8-mc");
+tg8tr.onclick = function(){
+  tg8m.style.display = "block";
+  tg8mc.src = this.src;
+}
+var tg8c = document.getElementById("tg-8-c");
+tg8c.onclick = function() {
+  tg8m.style.display = "none";
+}
+tg8m.onclick = function() {
+  tg8m.style.display = "none";
+}
+
+var tg9dm = document.getElementById("tg-9d-m");
+var tg9dtr = document.getElementById("tg-9d-tr");
+var tg9dmc = document.getElementById("tg-9d-mc");
+tg9dtr.onclick = function(){
+  tg9dm.style.display = "block";
+  tg9dmc.src = this.src;
+}
+var tg9dc = document.getElementById("tg-9d-c");
+tg9dc.onclick = function() {
+  tg9dm.style.display = "none";
+}
+tg9dm.onclick = function() {
+  tg9dm.style.display = "none";
+}
+
+var tg15m = document.getElementById("tg-15-m");
+var tg15tr = document.getElementById("tg-15-tr");
+var tg15mc = document.getElementById("tg-15-mc");
+tg15tr.onclick = function(){
+  tg15m.style.display = "block";
+  tg15mc.src = this.src;
+}
+var tg15c = document.getElementById("tg-15-c");
+tg15c.onclick = function() {
+  tg15m.style.display = "none";
+}
+tg15m.onclick = function() {
+  tg15m.style.display = "none";
+}
+
+var tg19m = document.getElementById("tg-19-m");
+var tg19tr = document.getElementById("tg-19-tr");
+var tg19mc = document.getElementById("tg-19-mc");
+tg19tr.onclick = function(){
+  tg19m.style.display = "block";
+  tg19mc.src = this.src;
+}
+var tg19c = document.getElementById("tg-19-c");
+tg19c.onclick = function() {
+  tg19m.style.display = "none";
+}
+tg19m.onclick = function() {
+  tg19m.style.display = "none";
+}
