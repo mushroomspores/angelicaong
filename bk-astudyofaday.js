401cf02b2beb5beb672c82ee8dc9e481fbb3c2eb
Angelica <angelicahui@gmail.com>
2022-11-22 00:20:37 -0600

    asoad book done

---
+++ bk-astudyofaday.js
@@ -0,0 +1,78 @@
+AOS.init({
+  duration: 900,
+})
+
+var asoad5m = document.getElementById("asoad-5-m");
+var asoad5tr = document.getElementById("asoad-5-tr");
+var asoad5mc = document.getElementById("asoad-5-mc");
+asoad5tr.onclick = function(){
+  asoad5m.style.display = "block";
+  asoad5mc.src = this.src;
+}
+var asoad5c = document.getElementById("asoad-5-c");
+asoad5c.onclick = function() {
+  asoad5m.style.display = "none";
+}
+asoad5m.onclick = function() {
+  asoad5m.style.display = "none";
+}
+
+var asoad6m = document.getElementById("asoad-6-m");
+var asoad6tr = document.getElementById("asoad-6-tr");
+var asoad6mc = document.getElementById("asoad-6-mc");
+asoad6tr.onclick = function(){
+  asoad6m.style.display = "block";
+  asoad6mc.src = this.src;
+}
+var asoad6c = document.getElementById("asoad-6-c");
+asoad6c.onclick = function() {
+  asoad6m.style.display = "none";
+}
+asoad6m.onclick = function() {
+  asoad6m.style.display = "none";
+}
+
+var asoad1m = document.getElementById("asoad-1-m");
+var asoad1tr = document.getElementById("asoad-1-tr");
+var asoad1mc = document.getElementById("asoad-1-mc");
+asoad1tr.onclick = function(){
+  asoad1m.style.display = "block";
+  asoad1mc.src = this.src;
+}
+var asoad1c = document.getElementById("asoad-1-c");
+asoad1c.onclick = function() {
+  asoad1m.style.display = "none";
+}
+asoad1m.onclick = function() {
+  asoad1m.style.display = "none";
+}
+
+var asoad4m = document.getElementById("asoad-4-m");
+var asoad4tr = document.getElementById("asoad-4-tr");
+var asoad4mc = document.getElementById("asoad-4-mc");
+asoad4tr.onclick = function(){
+  asoad4m.style.display = "block";
+  asoad4mc.src = this.src;
+}
+var asoad4c = document.getElementById("asoad-4-c");
+asoad4c.onclick = function() {
+  asoad4m.style.display = "none";
+}
+asoad4m.onclick = function() {
+  asoad4m.style.display = "none";
+}
+
+var asoad8m = document.getElementById("asoad-8-m");
+var asoad8tr = document.getElementById("asoad-8-tr");
+var asoad8mc = document.getElementById("asoad-8-mc");
+asoad8tr.onclick = function(){
+  asoad8m.style.display = "block";
+  asoad8mc.src = this.src;
+}
+var asoad8c = document.getElementById("asoad-8-c");
+asoad8c.onclick = function() {
+  asoad8m.style.display = "none";
+}
+asoad8m.onclick = function() {
+  asoad8m.style.display = "none";
+}
