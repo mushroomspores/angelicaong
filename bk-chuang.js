dbb562eb1c3bf659f9298ba336f65522a0242aa2
Angelica <angelicahui@gmail.com>
2022-12-22 22:40:19 -0600

    chuang page made

---
+++ bk-chuang.js
@@ -0,0 +1,123 @@
+AOS.init({
+  duration: 900,
+})
+
+var chuang11m = document.getElementById("chuang-11-m");
+var chuang11tr = document.getElementById("chuang-11-tr");
+var chuang11mc = document.getElementById("chuang-11-mc");
+chuang11tr.onclick = function(){
+  chuang11m.style.display = "block";
+  chuang11mc.src = this.src;
+}
+var chuang11c = document.getElementById("chuang-11-c");
+chuang11c.onclick = function() {
+  chuang11m.style.display = "none";
+}
+chuang11m.onclick = function() {
+  chuang11m.style.display = "none";
+}
+
+var chuang2m = document.getElementById("chuang-2-m");
+var chuang2tr = document.getElementById("chuang-2-tr");
+var chuang2mc = document.getElementById("chuang-2-mc");
+chuang2tr.onclick = function(){
+  chuang2m.style.display = "block";
+  chuang2mc.src = this.src;
+}
+var chuang2c = document.getElementById("chuang-2-c");
+chuang2c.onclick = function() {
+  chuang2m.style.display = "none";
+}
+chuang2m.onclick = function() {
+  chuang2m.style.display = "none";
+}
+
+var chuang3m = document.getElementById("chuang-3-m");
+var chuang3tr = document.getElementById("chuang-3-tr");
+var chuang3mc = document.getElementById("chuang-3-mc");
+chuang3tr.onclick = function(){
+  chuang3m.style.display = "block";
+  chuang3mc.src = this.src;
+}
+var chuang3c = document.getElementById("chuang-3-c");
+chuang3c.onclick = function() {
+  chuang3m.style.display = "none";
+}
+chuang3m.onclick = function() {
+  chuang3m.style.display = "none";
+}
+
+var chuang4m = document.getElementById("chuang-4-m");
+var chuang4tr = document.getElementById("chuang-4-tr");
+var chuang4mc = document.getElementById("chuang-4-mc");
+chuang4tr.onclick = function(){
+  chuang4m.style.display = "block";
+  chuang4mc.src = this.src;
+}
+var chuang4c = document.getElementById("chuang-4-c");
+chuang4c.onclick = function() {
+  chuang4m.style.display = "none";
+}
+chuang4m.onclick = function() {
+  chuang4m.style.display = "none";
+}
+
+var chuang5m = document.getElementById("chuang-5-m");
+var chuang5tr = document.getElementById("chuang-5-tr");
+var chuang5mc = document.getElementById("chuang-5-mc");
+chuang5tr.onclick = function(){
+  chuang5m.style.display = "block";
+  chuang5mc.src = this.src;
+}
+var chuang5c = document.getElementById("chuang-5-c");
+chuang5c.onclick = function() {
+  chuang5m.style.display = "none";
+}
+chuang5m.onclick = function() {
+  chuang5m.style.display = "none";
+}
+
+var chuang8m = document.getElementById("chuang-8-m");
+var chuang8tr = document.getElementById("chuang-8-tr");
+var chuang8mc = document.getElementById("chuang-8-mc");
+chuang8tr.onclick = function(){
+  chuang8m.style.display = "block";
+  chuang8mc.src = this.src;
+}
+var chuang8c = document.getElementById("chuang-8-c");
+chuang8c.onclick = function() {
+  chuang8m.style.display = "none";
+}
+chuang8m.onclick = function() {
+  chuang8m.style.display = "none";
+}
+
+var chuang9m = document.getElementById("chuang-9-m");
+var chuang9tr = document.getElementById("chuang-9-tr");
+var chuang9mc = document.getElementById("chuang-9-mc");
+chuang9tr.onclick = function(){
+  chuang9m.style.display = "block";
+  chuang9mc.src = this.src;
+}
+var chuang9c = document.getElementById("chuang-9-c");
+chuang9c.onclick = function() {
+  chuang9m.style.display = "none";
+}
+chuang9m.onclick = function() {
+  chuang9m.style.display = "none";
+}
+
+var chuang10m = document.getElementById("chuang-10-m");
+var chuang10tr = document.getElementById("chuang-10-tr");
+var chuang10mc = document.getElementById("chuang-10-mc");
+chuang10tr.onclick = function(){
+  chuang10m.style.display = "block";
+  chuang10mc.src = this.src;
+}
+var chuang10c = document.getElementById("chuang-10-c");
+chuang10c.onclick = function() {
+  chuang10m.style.display = "none";
+}
+chuang10m.onclick = function() {
+  chuang10m.style.display = "none";
+}
