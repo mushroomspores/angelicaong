5b8c883d92f027ebcf66ec3467d8af11630075bf
Angelica <angelicahui@gmail.com>
2022-12-29 17:17:05 -0600

    dr-thunder done

---
+++ dr-thunder.js
@@ -0,0 +1,82 @@
+AOS.init({
+  duration: 900,
+})
+//js script for ph-veneer.html
+// Get the modal
+var drthunder1m = document.getElementById("dr-thunder-1-m");
+// Get the image and insert it indrthunderde the modal - use its "alt" text as a caption
+var drthunder1tr = document.getElementById("dr-thunder-1-tr");
+var drthunder1mc = document.getElementById("dr-thunder-1-mc");
+drthunder1tr.onclick = function(){
+  drthunder1m.style.display = "block";
+  drthunder1mc.src = this.src;
+}
+// Get the <span> element that closes the modal
+var drthunder1c = document.getElementById("dr-thunder-1-c");
+// When the user clicks on <span> (x), close the modal
+drthunder1c.onclick = function() {
+  drthunder1m.style.display = "none";
+}
+drthunder1m.onclick = function() {
+  drthunder1m.style.display = "none";
+}
+
+var drthunder2m = document.getElementById("dr-thunder-2-m");
+var drthunder2tr = document.getElementById("dr-thunder-2-tr");
+var drthunder2mc = document.getElementById("dr-thunder-2-mc");
+drthunder2tr.onclick = function(){
+  drthunder2m.style.display = "block";
+  drthunder2mc.src = this.src;
+}
+var drthunder2c = document.getElementById("dr-thunder-2-c");
+drthunder2c.onclick = function() {
+  drthunder2m.style.display = "none";
+}
+drthunder2m.onclick = function() {
+  drthunder2m.style.display = "none";
+}
+
+var drthunder3m = document.getElementById("dr-thunder-3-m");
+var drthunder3tr = document.getElementById("dr-thunder-3-tr");
+var drthunder3mc = document.getElementById("dr-thunder-3-mc");
+drthunder3tr.onclick = function(){
+  drthunder3m.style.display = "block";
+  drthunder3mc.src = this.src;
+}
+var drthunder3c = document.getElementById("dr-thunder-3-c");
+drthunder3c.onclick = function() {
+  drthunder3m.style.display = "none";
+}
+drthunder3m.onclick = function() {
+  drthunder3m.style.display = "none";
+}
+
+var drthunder4m = document.getElementById("dr-thunder-4-m");
+var drthunder4tr = document.getElementById("dr-thunder-4-tr");
+var drthunder4mc = document.getElementById("dr-thunder-4-mc");
+drthunder4tr.onclick = function(){
+  drthunder4m.style.display = "block";
+  drthunder4mc.src = this.src;
+}
+var drthunder4c = document.getElementById("dr-thunder-4-c");
+drthunder4c.onclick = function() {
+  drthunder4m.style.display = "none";
+}
+drthunder4m.onclick = function() {
+  drthunder4m.style.display = "none";
+}
+
+var drthunder5m = document.getElementById("dr-thunder-5-m");
+var drthunder5tr = document.getElementById("dr-thunder-5-tr");
+var drthunder5mc = document.getElementById("dr-thunder-5-mc");
+drthunder5tr.onclick = function(){
+  drthunder5m.style.display = "block";
+  drthunder5mc.src = this.src;
+}
+var drthunder5c = document.getElementById("dr-thunder-5-c");
+drthunder5c.onclick = function() {
+  drthunder5m.style.display = "none";
+}
+drthunder5m.onclick = function() {
+  drthunder5m.style.display = "none";
+}
