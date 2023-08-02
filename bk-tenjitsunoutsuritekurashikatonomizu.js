3bda9d1352a047dc9528a3ef39b191c9daba7641
Angelica <angelicahui@gmail.com>
2023-08-02 23:07:16 +0800

    tenjitsu box page done and menu changes

--- bk-tenjitsunoutsuritekurashikatonomizu.js
+++ bk-tenjitsunoutsuritekurashikatonomizu.js
@@ -2,19 +2,34 @@ AOS.init({
   duration: 900,
 })
 
-var tenjitsu12m = document.getElementById("tenjitsu-12-m");
-var tenjitsu12tr = document.getElementById("tenjitsu-12-tr");
-var tenjitsu12mc = document.getElementById("tenjitsu-12-mc");
-tenjitsu12tr.onclick = function(){
-  tenjitsu12m.style.display = "block";
-  tenjitsu12mc.src = this.src;
+var tenjitsu1m = document.getElementById("tenjitsu-1-m");
+var tenjitsu1tr = document.getElementById("tenjitsu-1-tr");
+var tenjitsu1mc = document.getElementById("tenjitsu-1-mc");
+tenjitsu1tr.onclick = function(){
+  tenjitsu1m.style.display = "block";
+  tenjitsu1mc.src = this.src;
 }
-var tenjitsu12c = document.getElementById("tenjitsu-12-c");
-tenjitsu12c.onclick = function() {
-  tenjitsu12m.style.display = "none";
+var tenjitsu1c = document.getElementById("tenjitsu-1-c");
+tenjitsu1c.onclick = function() {
+  tenjitsu1m.style.display = "none";
 }
-tenjitsu12m.onclick = function() {
-  tenjitsu12m.style.display = "none";
+tenjitsu1m.onclick = function() {
+  tenjitsu1m.style.display = "none";
+}
+
+var tenjitsu2m = document.getElementById("tenjitsu-2-m");
+var tenjitsu2tr = document.getElementById("tenjitsu-2-tr");
+var tenjitsu2mc = document.getElementById("tenjitsu-2-mc");
+tenjitsu2tr.onclick = function(){
+  tenjitsu2m.style.display = "block";
+  tenjitsu2mc.src = this.src;
+}
+var tenjitsu2c = document.getElementById("tenjitsu-2-c");
+tenjitsu2c.onclick = function() {
+  tenjitsu2m.style.display = "none";
+}
+tenjitsu2m.onclick = function() {
+  tenjitsu2m.style.display = "none";
 }
 
 var tenjitsu3m = document.getElementById("tenjitsu-3-m");
@@ -75,64 +90,4 @@ tenjitsu6c.onclick = function() {
 }
 tenjitsu6m.onclick = function() {
   tenjitsu6m.style.display = "none";
-}
-
-var tenjitsu8m = document.getElementById("tenjitsu-8-m");
-var tenjitsu8tr = document.getElementById("tenjitsu-8-tr");
-var tenjitsu8mc = document.getElementById("tenjitsu-8-mc");
-tenjitsu8tr.onclick = function(){
-  tenjitsu8m.style.display = "block";
-  tenjitsu8mc.src = this.src;
-}
-var tenjitsu8c = document.getElementById("tenjitsu-8-c");
-tenjitsu8c.onclick = function() {
-  tenjitsu8m.style.display = "none";
-}
-tenjitsu8m.onclick = function() {
-  tenjitsu8m.style.display = "none";
-}
-
-var tenjitsu9m = document.getElementById("tenjitsu-9-m");
-var tenjitsu9tr = document.getElementById("tenjitsu-9-tr");
-var tenjitsu9mc = document.getElementById("tenjitsu-9-mc");
-tenjitsu9tr.onclick = function(){
-  tenjitsu9m.style.display = "block";
-  tenjitsu9mc.src = this.src;
-}
-var tenjitsu9c = document.getElementById("tenjitsu-9-c");
-tenjitsu9c.onclick = function() {
-  tenjitsu9m.style.display = "none";
-}
-tenjitsu9m.onclick = function() {
-  tenjitsu9m.style.display = "none";
-}
-
-var tenjitsu10m = document.getElementById("tenjitsu-10-m");
-var tenjitsu10tr = document.getElementById("tenjitsu-10-tr");
-var tenjitsu10mc = document.getElementById("tenjitsu-10-mc");
-tenjitsu10tr.onclick = function(){
-  tenjitsu10m.style.display = "block";
-  tenjitsu10mc.src = this.src;
-}
-var tenjitsu10c = document.getElementById("tenjitsu-10-c");
-tenjitsu10c.onclick = function() {
-  tenjitsu10m.style.display = "none";
-}
-tenjitsu10m.onclick = function() {
-  tenjitsu10m.style.display = "none";
-}
-
-var tenjitsu11m = document.getElementById("tenjitsu-11-m");
-var tenjitsu11tr = document.getElementById("tenjitsu-11-tr");
-var tenjitsu11mc = document.getElementById("tenjitsu-11-mc");
-tenjitsu11tr.onclick = function(){
-  tenjitsu11m.style.display = "block";
-  tenjitsu11mc.src = this.src;
-}
-var tenjitsu11c = document.getElementById("tenjitsu-11-c");
-tenjitsu11c.onclick = function() {
-  tenjitsu11m.style.display = "none";
-}
-tenjitsu11m.onclick = function() {
-  tenjitsu11m.style.display = "none";
 }
