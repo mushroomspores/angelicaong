15fa554c595fcea4591e793d4b8054cf1b4cb397
Angelica <angelicahui@gmail.com>
2023-08-06 17:51:13 +0800

    menu js updated

--- bk-shimonokoe-heavenlybodies.js
+++ bk-shimonokoe-heavenlybodies.js
@@ -2,6 +2,22 @@ AOS.init({
   duration: 900,
 })
 
+var phoneDropbtn = document.getElementById("phonedrop-btn")
+phoneDropbtn.addEventListener("click", myFunction);
+
+function myFunction() {
+  var phoneDrop = document.getElementById("phone-droplist");
+  var slideshow = document.getElementById("landing-page-slideshow")
+  if (phoneDrop.style.display == '' || phoneDrop.style.display == "none") {
+    phoneDrop.style.display = "block";
+    slideshow.style.display = "none";
+  } else {
+    phoneDrop.style.display = "none";
+    slideshow.style.display = "block";
+  }
+}
+
+
 var snkhb1m = document.getElementById("snk-hb-1-m");
 var snkhb1tr = document.getElementById("snk-hb-1-tr");
 var snkhb1mc = document.getElementById("snk-hb-1-mc");
