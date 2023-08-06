17864d98a9097389fd87c7c11606b2e6bef56216
Angelica <angelicahui@gmail.com>
2023-08-06 23:11:04 +0800

    all book pages done!!

--- bk-minutetremblings.js
+++ bk-minutetremblings.js
@@ -2,6 +2,21 @@ AOS.init({
   duration: 900,
 })
 
+var phoneDropbtn = document.getElementById("phonedrop-btn")
+phoneDropbtn.addEventListener("click", myFunction);
+
+function myFunction() {
+  var phoneDrop = document.getElementById("phone-droplist");
+  var slideshow = document.getElementById("landing-page-slideshow")
+  if (phoneDrop.style.display == '' || phoneDrop.style.display == "none") {
+    phoneDrop.style.display = "block";
+    slideshow.style.display = "none";
+  } else {
+    phoneDrop.style.display = "none";
+    slideshow.style.display = "block";
+  }
+}
+
 var mt1m = document.getElementById("mt-1-m");
 var mt1tr = document.getElementById("mt-1-tr");
 var mt1mc = document.getElementById("mt-1-mc");
--- bk-stilledimages.js
+++ bk-stilledimages.js
@@ -2,6 +2,21 @@ AOS.init({
   duration: 900,
 })
 
+var phoneDropbtn = document.getElementById("phonedrop-btn")
+phoneDropbtn.addEventListener("click", myFunction);
+
+function myFunction() {
+  var phoneDrop = document.getElementById("phone-droplist");
+  var slideshow = document.getElementById("landing-page-slideshow")
+  if (phoneDrop.style.display == '' || phoneDrop.style.display == "none") {
+    phoneDrop.style.display = "block";
+    slideshow.style.display = "none";
+  } else {
+    phoneDrop.style.display = "none";
+    slideshow.style.display = "block";
+  }
+}
+
 var bksi3m = document.getElementById("bk-si-3-m");
 var bksi3tr = document.getElementById("bk-si-3-tr");
 var bksi3mc = document.getElementById("bk-si-3-mc");
