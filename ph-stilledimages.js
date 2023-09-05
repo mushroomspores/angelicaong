272c50ccf239a471d0dabe988eb106d2acf75357
Angelica <angelicahui@gmail.com>
2023-09-05 19:00:44 +0800

    photo mobile pages all done

--- ph-stilledimages.js
+++ ph-stilledimages.js
@@ -1,6 +1,21 @@
 AOS.init({
   duration: 900,
 })
+
+var phoneDropbtn = document.getElementById("phonedrop-btn")
+phoneDropbtn.addEventListener("click", myFunction);
+
+function myFunction() {
+  var phoneDrop = document.getElementById("phone-droplist");
+  var slideshow = document.getElementById("landing-page-slideshow")
+  if (phoneDrop.style.display == '' || phoneDrop.style.display == "none") {
+    phoneDrop.style.display = "block";
+    slideshow.style.display = "none";
+  } else {
+    phoneDrop.style.display = "none";
+    slideshow.style.display = "block";
+  }
+}
 //js script for ph-veneer.html
 // Get the modal
 var si1m = document.getElementById("si-1-m");
