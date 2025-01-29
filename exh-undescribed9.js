279599e6fbb3371ec27b1d70b27be3dd1bc3e7b2
mushroomspores <angelicahui@gmail.com>
2025-01-30 03:06:47 +0800

    exhibition and cv updates

---
+++ exh-undescribed9.js
@@ -0,0 +1,138 @@
+AOS.init({
+    duration: 900,
+  })
+  
+  var phoneDropbtn = document.getElementById("phonedrop-btn")
+  phoneDropbtn.addEventListener("click", myFunction);
+  
+  function myFunction() {
+    var phoneDrop = document.getElementById("phone-droplist");
+    var slideshow = document.getElementById("landing-page-slideshow")
+    if (phoneDrop.style.display == '' || phoneDrop.style.display == "none") {
+      phoneDrop.style.display = "block";
+      slideshow.style.display = "none";
+    } else {
+      phoneDrop.style.display = "none";
+      slideshow.style.display = "block";
+    }
+  }
+  
+  var ud9exh1m = document.getElementById("ud9exh-1-m");
+  var ud9exh1tr = document.getElementById("ud9exh-1-tr");
+  var ud9exh1mc = document.getElementById("ud9exh-1-mc");
+  ud9exh1tr.onclick = function(){
+    ud9exh1m.style.display = "block";
+    ud9exh1mc.src = this.src;
+  }
+  var ud9exh1c = document.getElementById("ud9exh-1-c");
+  ud9exh1c.onclick = function() {
+    ud9exh1m.style.display = "none";
+  }
+  ud9exh1m.onclick = function() {
+    ud9exh1m.style.display = "none";
+  }
+  
+  var ud9exh2m = document.getElementById("ud9exh-2-m");
+  var ud9exh2tr = document.getElementById("ud9exh-2-tr");
+  var ud9exh2mc = document.getElementById("ud9exh-2-mc");
+  ud9exh2tr.onclick = function(){
+    ud9exh2m.style.display = "block";
+    ud9exh2mc.src = this.src;
+  }
+  var ud9exh2c = document.getElementById("ud9exh-2-c");
+  ud9exh2c.onclick = function() {
+    ud9exh2m.style.display = "none";
+  }
+  ud9exh2m.onclick = function() {
+    ud9exh2m.style.display = "none";
+  }
+  
+  var ud9exh3m = document.getElementById("ud9exh-3-m");
+  var ud9exh3tr = document.getElementById("ud9exh-3-tr");
+  var ud9exh3mc = document.getElementById("ud9exh-3-mc");
+  ud9exh3tr.onclick = function(){
+    ud9exh3m.style.display = "block";
+    ud9exh3mc.src = this.src;
+  }
+  var ud9exh3c = document.getElementById("ud9exh-3-c");
+  ud9exh3c.onclick = function() {
+    ud9exh3m.style.display = "none";
+  }
+  ud9exh3m.onclick = function() {
+    ud9exh3m.style.display = "none";
+  }
+  
+  var ud9exh4m = document.getElementById("ud9exh-4-m");
+  var ud9exh4tr = document.getElementById("ud9exh-4-tr");
+  var ud9exh4mc = document.getElementById("ud9exh-4-mc");
+  ud9exh4tr.onclick = function(){
+    ud9exh4m.style.display = "block";
+    ud9exh4mc.src = this.src;
+  }
+  var ud9exh4c = document.getElementById("ud9exh-4-c");
+  ud9exh4c.onclick = function() {
+    ud9exh4m.style.display = "none";
+  }
+  ud9exh4m.onclick = function() {
+    ud9exh4m.style.display = "none";
+  }
+  
+  var ud9exh5m = document.getElementById("ud9exh-5-m");
+  var ud9exh5tr = document.getElementById("ud9exh-5-tr");
+  var ud9exh5mc = document.getElementById("ud9exh-5-mc");
+  ud9exh5tr.onclick = function(){
+    ud9exh5m.style.display = "block";
+    ud9exh5mc.src = this.src;
+  }
+  var ud9exh5c = document.getElementById("ud9exh-5-c");
+  ud9exh5c.onclick = function() {
+    ud9exh5m.style.display = "none";
+  }
+  ud9exh5m.onclick = function() {
+    ud9exh5m.style.display = "none";
+  }
+  
+  var ud9exh6m = document.getElementById("ud9exh-6-m");
+  var ud9exh6tr = document.getElementById("ud9exh-6-tr");
+  var ud9exh6mc = document.getElementById("ud9exh-6-mc");
+  ud9exh6tr.onclick = function(){
+    ud9exh6m.style.display = "block";
+    ud9exh6mc.src = this.src;
+  }
+  var ud9exh6c = document.getElementById("ud9exh-6-c");
+  ud9exh6c.onclick = function() {
+    ud9exh6m.style.display = "none";
+  }
+  ud9exh6m.onclick = function() {
+    ud9exh6m.style.display = "none";
+  }
+  
+  var ud9exh7m = document.getElementById("ud9exh-7-m");
+  var ud9exh7tr = document.getElementById("ud9exh-7-tr");
+  var ud9exh7mc = document.getElementById("ud9exh-7-mc");
+  ud9exh7tr.onclick = function(){
+    ud9exh7m.style.display = "block";
+    ud9exh7mc.src = this.src;
+  }
+  var ud9exh7c = document.getElementById("ud9exh-7-c");
+  ud9exh7c.onclick = function() {
+    ud9exh7m.style.display = "none";
+  }
+  ud9exh7m.onclick = function() {
+    ud9exh7m.style.display = "none";
+  }
+  
+  var ud9exh8m = document.getElementById("ud9exh-8-m");
+  var ud9exh8tr = document.getElementById("ud9exh-8-tr");
+  var ud9exh8mc = document.getElementById("ud9exh-8-mc");
+  ud9exh8tr.onclick = function(){
+    ud9exh8m.style.display = "block";
+    ud9exh8mc.src = this.src;
+  }
+  var ud9exh8c = document.getElementById("ud9exh-8-c");
+  ud9exh8c.onclick = function() {
+    ud9exh8m.style.display = "none";
+  }
+  ud9exh8m.onclick = function() {
+    ud9exh8m.style.display = "none";
+  }
